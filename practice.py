b82b3e1d216914407b72414949331007030e804b
nickovilloria <nickovilloria@hotmail.com>
2025-07-25 23:01:21 -0500

    """Changes made to files as a part of practicing python skills"""

--- practice.py
+++ practice.py
@@ -1,18 +1,5 @@
-def has_duplicates(arr):
-    counts = {}  # Dictionary to store counts of each number
+import pandas as pd
 
-    for num in arr:
-        if num in counts:
-            counts[num] += 1  # Increment count if number is already in the dictionary
-        else:
-            counts[num] = 1  # Initialize count if number is not in the dictionary
-
-    # Filter out numbers that are not repeated
-    repeated = {num: count for num, count in counts.items() if count > 1}
-    
-    return repeated
-
-# Get input and convert it to a list of integers
-arr = list(map(int, input('Provide a list of integers separated by commas: ').split(',')))
-result = has_duplicates(arr)
-print(result)
+data={'amount':[100,1100,2000],'date':['2021-08-12','2024-10-09','2025-03-24']}
+tns= pd.DataFrame(data)
+print(tns)
